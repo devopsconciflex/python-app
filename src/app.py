1a82f9c4cc587cf7d6921cea48683f68205ad726
fvilasuolinc <fvilas@uolinc.com>
2025-05-04 21:46:41 -0300

    remove message from app

--- src/app.py
+++ src/app.py
@@ -11,7 +11,6 @@ def details():
         {
             'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
             'hostname': socket.gethostname(),
-            'message': 'You are doing great, little human! <3',
             'deployed_on': 'kubernetes'
         }
     )
