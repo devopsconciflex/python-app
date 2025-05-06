9a4c6cf5efd616280725c82c29f8dccb49b64da0
fvilasuolinc <fvilas@uolinc.com>
2025-05-05 22:56:11 -0300

    new message

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         {
             'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
             'hostname': socket.gethostname(),
-            'message' : "That was cool! The image was build and pushed through Git Actions"
+            'message' : "The image was build and pushed through Git Actions"
             'deployed_on': 'kubernetes'
         }
     )
